26399a0dce78f3452babdf5e791a9cff5bec3699
romaintailhurat <romain.tailhurat@gmail.com>
2017-03-21 11:19:55 +0100

    Adding proper logging, refactoring a bit the main class

--- src/main/java/fr/insee/eno/cli/Run.java
+++ src/main/java/fr/insee/eno/cli/Run.java
@@ -1,35 +1,69 @@
 package fr.insee.eno.cli;
 
+import java.io.File;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import fr.insee.eno.DDI2FRContext;
+import fr.insee.eno.GenerationService;
 
 /**
  * Defines the command-line interface to Eno.
  * A basic scenario is to produce an XForms questionnaire from a DDI specification.
  * */
 public class Run {
+	
+	private static final Logger logger = LogManager.getLogger(Run.class);
+	
+	// FIXME temporary
+	private static final String SIMPSONS_TEST_PATH = "D:/__TEMP/eno-test/simpsons.xml"; 
 
 	public static void main(String[] args) {
-		System.out.println("> Starting <");
+		logger.info("> Start <");
 		Options options = defineOptions();
-		
-		HelpFormatter formatter = new HelpFormatter();		
-		
-		CommandLineParser parser = new DefaultParser();
-		
+		CommandLineParser parser = new DefaultParser();		
 		try {
-			CommandLine cli = parser.parse(options, args);
-			if(cli.hasOption("h")) {
-				formatter.printHelp("Eno-CLI", options);
-			}
+			launchGeneration(args, options, parser);
 		} catch (ParseException e) {
-			System.out.println("Error when parsing command: " + e.getMessage());
-		}
+			logger.error("Error when parsing command: " + e.getMessage());
+		} catch (Exception e) {
+			logger.error("Generic exception: " + e.getMessage());
+		}		
+		System.out.println("> End <");
+	}
 
+	/**
+	 * @param args
+	 * @param options
+	 * @param parser
+	 * @throws ParseException
+	 * @throws Exception
+	 */
+	private static void launchGeneration(String[] args, Options options, CommandLineParser parser)
+			throws ParseException, Exception {
+		CommandLine cli = parser.parse(options, args);
+		if(cli.hasOption("h")) {
+			printHelp(options);
+		}
+		Injector injector = Guice.createInjector(new DDI2FRContext());
+		GenerationService service = injector.getInstance(GenerationService.class);
+		File generatedFile = service.generateQuestionnaire(new File(SIMPSONS_TEST_PATH), null);
+		logger.info("File generated successfully: " + generatedFile);
+	}
+
+	private static void printHelp(Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("Eno-CLI", options);
 	}
 
 	private static Options defineOptions() {
